deb4c24ffaa9514466bb4a47e849d508b2ad1cea
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-10-01 11:55:40 +0500

    feat: solve brackets rask

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let localStr = str;
+
+    let prevStrLen = localStr.length + 1;
+
+    while (localStr.length < prevStrLen) {
+        prevStrLen = localStr.length;
+
+        bracketsConfig.forEach(config => {
+
+            console.log(config);
+
+            localStr = localStr.split(config[0] + config[1]).join('');
+
+            console.log(localStr);
+        });
+    }
+
+    return localStr.length === 0;
 }
